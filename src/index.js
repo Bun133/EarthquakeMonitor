fc97286295741468454e8eae0a1c7ba23b1258d9
Bun133 <57163334+Bun133@users.noreply.github.com>
2020-06-04 11:15:22 +0900

    update
    update

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const parser = require('fast-xml-parser')
 const request = require('request');
 const encoding = require('encoding-japanese');
 const https=require('https');
+const get_json=require('get-json');
 let win;
 
 const createWindows = () => {
@@ -24,25 +25,64 @@ ipcMain.on('get',(event,arg) => {
     url : 'https://api.p2pquake.net/v1/human-readable',
     method : 'GET'
   }
-  const req=https.request(options.url,(res)=>{
+
+  https.get(options.url, function (res) {
+    var body = '';
+    res.setEncoding('utf8');
+    res.on('data', function (chunk) {
+        console.log(JSON.parse(chunk));
+        event.sender.send('api-reply',JSON.parse(chunk));
+    });
+  }).on('error', function (e) {
+    console.log(e.message);
+});
+
+  /*get_json(options.url,(error,responce)=>{
+    console.log('Error:');
+    console.log(error);
+    console.log('Responce:');
+    // responce.setEncode('UTF-8');
+    console.log(responce);
+
+    console.log('UTF-8');
+    console.log(encoding.convert(responce,{to:'UTF-8',from:'AUTO',type:'string'}));
+
+    // console.log('Json');
+    // console.log(JSON.parse(responce));
+  });*/
+
+  /*const URL = "https://api.p2pquake.net/v1/human-readable";
+
+https.get(URL, function (res) {
+    var body = '';
+    res.setEncoding('utf8');
+    // res.header('Content-Type', 'text/plain;charset=utf-8');
+    res.on('data', function (chunk) {
+        console.log(chunk);
+    });
+  }).on('error', function (e) {
+    console.log(e.message);
+});*/
+
+
+  /*const req=https.request(options.url,(res)=>{
     res.setEncoding('UTF-8');
     res.on('data',(chunk)=>{
+      // chunk.setEncoding('UTF-8');
       console.log('Raw');
       console.log(chunk);
       console.log('UNICODE');
-      console.log(encoding.convert(chunk,{to:'UNICODE',from:'UTF-8',type:'string'}));
+      console.log(encoding.convert(chunk,{to:'UNICODE',from:'BINARY',type:'string'}));
       console.log('SJIS');
-      console.log(encoding.convert(chunk,{to:'SJIS',from:'UTF-8',type:'string'}));
+      console.log(encoding.convert(chunk,{to:'SJIS',from:'BINARY',type:'string'}));
       console.log('UTF-8');
-      console.log(encoding.convert(chunk,{to:'UTF-8',from:'UTF-8',type:'string'}));
-      console.log('Json:');
-      console.log(JSON.parse(chunk));
+      console.log(encoding.convert(chunk,{to:'UTF-8',from:'BINARY',type:'string'}));
     });
     res.on('end',()=>{
-      console.log('No Data');
+      console.log('END');
     });
   });
- req.end();
+ req.end();*/
 
 
   /*request('https://api.p2pquake.net/v1/human-readable?limit=1',(error,responce,body)=>{
--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,82 @@
 const electron = window.electron
 const ipcRenderer = electron.ipcRenderer
-const text=document.getElementsByClassName('text')
+
+function onRecived(jsondata){
+  console.log(getType(jsondata[0]['code']));
+  console.log(getScale(jsondata[0]['earthquake']['maxScale']));
+  console.log(getDomesticTunami(jsondata[0]['earthquake']['domesticTsunami']));
+  // console.log(jsondata[0]['points']);
+  printPoints(jsondata[0]['points']);
+  console.log(`この地震の最大震度は${getScale(jsondata[0]['earthquake']['maxScale'])}`);
+  console.log(`この地震の発生時刻は${jsondata[0]['earthquake']['time']}`);
+  console.log(getHypoCenterString(jsondata[0]['earthquake']['hypocenter']));
+}
+
+function scaleConvert(scale){
+  return scale/10;
+}
+
+function getScale(scale){
+  var shindo=0;
+  switch(scale){
+    case 45:
+      return '震度5弱';
+    case 50:
+      return '震度5強';
+    case 55:
+      return '震度6弱';
+    case 60:
+      return '震度6強';
+    default:
+      return `震度${scaleConvert(scale)}`;
+  }
+}
+
+function getDomesticTunami(tunamiData){
+  switch (tunamiData) {
+    case 'None':
+      return '津波の心配なし';
+    case 'Unknown':
+      return '津波は不明';
+    case 'Checking':
+      return '津波は調査中';
+    case 'NonEffective':
+     return '若干の海面変動[被害の心配なし]';
+    case 'Watch':
+      return '津波注意報';
+    case 'Warning':
+      return '津波予報[種類不明]';
+  }
+}
+
+function printPoints(arr){
+  for(var i=0;i<arr.length;i++){
+    console.log(`${arr[i]['addr']}:${getScale(arr[i]['scale'])}`);
+  }
+}
+
+function getHypoCenterString(hypoData){
+  return `震源地は${hypoData['name']}、緯度${hypoData['latitude']}、経度${hypoData['longitude']}、深さは${hypoData['depth']}、マグニチュードは${hypoData['magnitude']}`;
+}
+
+function getType(code){
+  switch(code){
+    case 551:
+      return '津波情報';
+      break;
+    case 552:
+      return '津波予報'
+      break;
+    case 5610:
+      return '集計済み地震感知情報'
+      break;
+  }
+}
+
 
 ipcRenderer.send('get','get')
 
 ipcRenderer.on('api-reply',(event,arg)=>{
   console.log(arg);
-  text.innerHTML=arg;
-})
+  onRecived(arg);
+});
